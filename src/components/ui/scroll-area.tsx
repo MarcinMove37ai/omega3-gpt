c648522f27462e045118fb83e2ca860ffb1ab976
MarcinMove37ai <marcinlisiak83@gmial.com>
2025-01-07 10:25:18 +0100

    Deploy fix_7

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -1,8 +1,9 @@
+'use client'
+
 import * as React from "react"
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
-
 import { cn } from "@/lib/utils"
-'use client'
+
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
