2debbd51b8cf6cdd20b90b243db84f121c4a51fd
MarcinMove37ai <marcinlisiak83@gmial.com>
2025-01-07 09:56:21 +0100

    Deploy fix_4

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
-  output: 'export',
+  output: 'standalone',
   distDir: '.next',
   images: {
     unoptimized: true
@@ -14,6 +14,17 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
     workerThreads: true
+  },
+  // Wyłączamy generowanie statycznych stron
+  staticPageGenerationTimeout: 0,
+  // Wymuszamy tryb serverowy dla wszystkich stron
+  rewrites: async () => {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+    ]
   }
 }
 
